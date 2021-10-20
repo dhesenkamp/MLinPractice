ee5dd54ec898adad64746d2852f686a301722680
Yannik101010 <80115275+Yannik101010@users.noreply.github.com>
2021-10-20 13:05:56 +0200

    Update likes.py
    implemented _get_values()

--- code/feature_extraction/likes.py
+++ code/feature_extraction/likes.py
@@ -5,11 +5,15 @@ Created on Tue Oct 19 18:01:56 2021
 @author: Yannik
 """
 
+from code.feature_extraction.feature_extractor import FeatureExtractor
+import numpy as np
+
 class LikeExtractor(FeatureExtractor):
-    """Collects the number of likes for a Tweet"""
+    """Collects the number of likes for a Tweet and stores them as seperate feature"""
     
     def __init__(self, input_column):
-        super.__init__(input_column, "likes")
+        super.__init__(input_column, "feature_likes")
         
     def _get_values(self, inputs):
-        
+        result = np.array(inputs[0])
+        return result.reshape(-1,1)
