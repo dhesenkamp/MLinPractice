78cf0551ee39e33b6ef0168ae4d2f5fcb89ed91c
Dennis Hesenkamp <83503798+dhesenkamp@users.noreply.github.com>
2021-10-21 10:42:57 +0200

    Command line args for URL() feature extractor

--- code/util.py
+++ code/util.py
@@ -14,6 +14,8 @@ COLUMN_LIKES = "likes_count"
 COLUMN_RETWEETS = "retweets_count"
 COLUMN_MONTH = "date"
 COLUMN_PHOTOS = "photos"
+COLUMN_MENTIONS = "mentions"
+COLUMN_URL = "urls"
 
 # column names of novel columns for preprocessing
 COLUMN_LABEL = "label"
