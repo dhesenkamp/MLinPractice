bab4cc7813823a74a944b0248ba22cde54fc3236
Yannik101010 <80115275+Yannik101010@users.noreply.github.com>
2021-10-26 13:10:56 +0200

    Update tf-idf
    Added set variables and extraction of the features/keywords

--- code/feature_extraction/tf-idf.py
+++ code/feature_extraction/tf-idf.py
@@ -17,11 +17,14 @@ class TfIdf(FeatureExtractor):
     
     def __init__(self, input_column):
         super.__init__([input_column], "{0}_Tf-Idf".format(input_column))
+        self.tfidf_vectorizer = TfidfVectorizer(max_features = 20)
+        
+    def _set_variables(self, inputs):
+        self.tfidf_vectorizer.fit(inputs[0])
         
     def _get_values(self, inputs):
-        vectorizer = TfidfVectorizer(max_features = 20)
-        result = vectorizer.transform(inputs[0])
+        result = self.tfidf_vectorizer.transform(inputs[0])
+        keywords = self.tfidf_vectorizer.get_feature_names()
         return np.array(result)
         # compute cosine similarity?
         # need to safe the # most occuring words as keywords with get_feature_names() (get_feature_names_out(input_features)???)
-        # fit_transform???
